354a649cf9960472d862bdc38946f65f9d6a0b84
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-12-15 15:49:29 +0530

    Footer component is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularMaterialsModule } from './modules/angular-materials/angular-materials.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
