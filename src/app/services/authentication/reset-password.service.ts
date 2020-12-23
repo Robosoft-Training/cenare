ec6b46601761c77985de14b29f58f634cdd8556b
premrobosoft <premnath@robosoftin.com>
2020-12-23 09:36:58 +0530

    Service created for Reset Password

---
+++ src/app/services/authentication/reset-password.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { LocalStorageService } from '../local-storage/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResetPasswordService {
+
+  baseUrl = environment.baseUrl;
+
+  constructor(
+    private httpClient: HttpClient,
+    private localStorageService: LocalStorageService
+  ) { }
+
+  resetPassWordSendOtp = (email): Observable<any[]> => {
+    console.log(email);
+    const postBody = {
+      email: email
+    };
+    this.localStorageService.setUserEmail(email);
+    const url = `${this.baseUrl}resetpassword/sendotp`;
+    return this.httpClient.post<any[]>(url, postBody).pipe(
+      tap(
+        (data) => {
+
+        }
+      )
+    );
+  }
+
+  resetPassword = (password): Observable<any[]> => {
+    const email = this.localStorageService.getUserEmail();
+    const postBody = {
+      email: email,
+      password: password
+    };
+    console.log(postBody);
+    const url = `${this.baseUrl}resetpassword`;
+    return this.httpClient.post<any[]>(url, postBody).pipe(
+      tap(
+        (data) => {
+
+        }
+      )
+    );
+  }
+}
