31c8fe4312785af0f075443d96b993661dfa48fb
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-12-25 11:22:57 +0530

    Reviews and Gallery

--- src/app/modules/menu-list/components/gallery/gallery.component.ts
+++ src/app/modules/menu-list/components/gallery/gallery.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  isOpen1 = false;
+  isOpen2 = false;
+  isOpen3 = false;
+
+
+  displayImage1() {
+    this.isOpen1 = !this.isOpen1;
+  }
+
+  close1() {
+    this.isOpen1 = !this.isOpen1;
+  }
+
+  displayImage2() {
+    this.isOpen2 = !this.isOpen2;
+  }
+
+  close2() {
+    this.isOpen2 = !this.isOpen2;
+  }
+
+  displayImage3() {
+    this.isOpen3 = !this.isOpen3;
+  }
+
+  close3() {
+    this.isOpen3 = !this.isOpen3;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
