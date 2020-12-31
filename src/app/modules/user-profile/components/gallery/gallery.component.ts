0d53060f9a08bce6303e26fd401e12a734d252af
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-01 04:55:27 +0530

    User gallery is created

---
+++ src/app/modules/user-profile/components/gallery/gallery.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent implements OnInit {
+
+  isOpen1 = false;
+  isOpen2 = false;
+  isOpen3 = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  displayImage1() {
+    this.isOpen1 = !this.isOpen1;
+  }
+
+  close1() {
+    this.isOpen1 = !this.isOpen1;
+  }
+
+  displayImage2() {
+    this.isOpen2 = !this.isOpen2;
+  }
+
+  close2() {
+    this.isOpen2 = !this.isOpen2;
+  }
+
+  displayImage3() {
+    this.isOpen3 = !this.isOpen3;
+  }
+
+  close3() {
+    this.isOpen3 = !this.isOpen3;
+  }
+
+}
