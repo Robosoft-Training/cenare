3ea8761cb7991ab6b0f4f4997ff8958ddac86535
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-01 13:27:04 +0530

    User Gallery updated

--- src/app/modules/user-profile/components/gallery/gallery.component.ts
+++ src/app/modules/user-profile/components/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-gallery',
@@ -8,15 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class GalleryComponent implements OnInit {
 
   isOpen1 = false;
-  isOpen2 = false;
-  isOpen3 = false;
+  clickedImage: any = null;
+  clickedCount = 0;
+
+  imageList = [
+    "../../../../../assets/images/Vada.jpg",
+    "../../../../../assets/images/TandooriChicken.jpg",
+    "../../../../../assets/images/uttapam.jpg"
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    $("#left").addClass('left');
+    $("#left").removeClass('leftArrow');
+    console.log(this.clickedCount);
   }
 
-  displayImage1() {
+  displayImage(imageUrl, count) {
+    this.clickedCount = count;
+    this.clickedImage = imageUrl;
     this.isOpen1 = !this.isOpen1;
   }
 
@@ -24,20 +36,51 @@ export class GalleryComponent implements OnInit {
     this.isOpen1 = !this.isOpen1;
   }
 
-  displayImage2() {
-    this.isOpen2 = !this.isOpen2;
+  leftArrow() {
+    console.log(this.clickedCount)
+    if (this.clickedCount === 0) {
+      $("#left").addClass('left');
+      $("#left").removeClass('leftArrow');
+      console.log(this.clickedCount);
+    }
+    else {
+      $("#left").removeClass('left');
+      $("#right").addClass('right');
+      $("#left").addClass('leftArrow');
+      this.clickedImage = this.imageList[--this.clickedCount];
+    }
   }
 
-  close2() {
-    this.isOpen2 = !this.isOpen2;
+  rightArrow() {
+    console.log(this.clickedCount)
+    if (this.clickedCount === this.imageList.length-1) {
+      $("#right").addClass('right');
+      $("#right").removeClass('rightArrow');
+      console.log(this.clickedCount);
+    }
+    else {
+      $("#right").removeClass('right');
+      $("#right").addClass('rightArrow');
+      this.clickedImage = this.imageList[++this.clickedCount];
+    }
   }
 
-  displayImage3() {
-    this.isOpen3 = !this.isOpen3;
-  }
+  // isOpen2 = false;
+  // isOpen3 = false;
+  // displayImage2() {
+  //   this.isOpen2 = !this.isOpen2;
+  // }
 
-  close3() {
-    this.isOpen3 = !this.isOpen3;
-  }
+  // close2() {
+  //   this.isOpen2 = !this.isOpen2;
+  // }
+
+  // displayImage3() {
+  //   this.isOpen3 = !this.isOpen3;
+  // }
+
+  // close3() {
+  //   this.isOpen3 = !this.isOpen3;
+  // }
 
 }
