b294024630f35e307572bbde357863d9bff6ed48
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-04 10:59:55 +0530

    Payment-methods updated

--- src/app/modules/user-profile/components/payment-methods/payment-methods.component.ts
+++ src/app/modules/user-profile/components/payment-methods/payment-methods.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddNewCardComponent } from '../add-new-card/add-new-card.component';
 
 @Component({
   selector: 'app-payment-methods',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentMethodsComponent implements OnInit {
 
-  constructor() { }
+  cards = {
+    icon: "",
+    cardNumber: "",
+    cardName: "",
+  } 
+
+  cardlist: any = [
+    {
+      icon: "../../../../../assets/images/visa_icon.png",
+      cardNumber: '2345 XXXX XXXX 6574',
+      cardName: 'Visa',
+    },
+    {
+      icon: "../../../../../assets/images/mastercard_icon.png",
+      cardNumber: '1234 XXXX XXXX 3456',
+      cardName: 'Master card',
+    }
+  ];
+
+  constructor(
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog(formType: any): void {
+    this.dialog.open(AddNewCardComponent, { panelClass: 'custom-dialog-container', data: { formType } });
   }
 
 }
