d7106cc60db32f85d03f62e4fe348fb248cc5de7
premrobosoft <premnath@robosoftin.com>
2021-01-07 13:01:35 +0530

    Page loader added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://ec2-13-235-42-221.ap-south-1.compute.amazonaws.com:8080/',
+  apiKey: 'AVVcB8jjfmf7eYftCZAdiyFzAyMnrVLO',
+  getAdressBaseUrl: 'https://api.tomtom.com/search/2/reverseGeocode/',
+  getLatitudeLongitudeBaseUrl: 'https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=IN&limit=1&streetName='
 };
