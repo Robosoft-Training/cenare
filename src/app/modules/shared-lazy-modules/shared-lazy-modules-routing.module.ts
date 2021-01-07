c9745358ba1a1a42e167b26baef32cd66ce6187e
premrobosoft <premnath@robosoftin.com>
2021-01-07 09:13:03 +0530

    Image relative path removed

--- src/app/modules/shared-lazy-modules/shared-lazy-modules-routing.module.ts
+++ src/app/modules/shared-lazy-modules/shared-lazy-modules-routing.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NearbyBrandsComponent } from './components/nearby-brands/nearby-brands.component';
 
-import { SharedLazyModulesComponent } from './shared-lazy-modules.component';
-
 const routes: Routes = [
-  { path: '', component: SharedLazyModulesComponent },
-  { path: 'nearby-brands', component: NearbyBrandsComponent }
+  { path: '', component: NearbyBrandsComponent }
 ];
 
 @NgModule({
