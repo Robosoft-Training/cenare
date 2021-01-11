e586ec0843ab776e1eaec94b7a8cc09422d54031
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-11 13:45:58 +0530

    add-new-card component updated

--- src/app/modules/user-profile/components/add-new-card/add-new-card.component.ts
+++ src/app/modules/user-profile/components/add-new-card/add-new-card.component.ts
@@ -8,6 +8,12 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class AddNewCardComponent implements OnInit {
 
+  cvv = { value: '', error: '' };
+  name = { value: '', error: '' };
+  cardNumber = { value: '', error: '' };
+  expiryMonth = { value: '', error: '' };
+  expiryYear = { value: '', error: '' };
+
   constructor(
     public dialogRef: MatDialogRef<AddNewCardComponent>
   ) { }
@@ -19,4 +25,33 @@ export class AddNewCardComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  saveCard(): void {
+    if (!this.cardNumber.value) {
+      this.cardNumber.error = 'Card number is required';
+    }
+    else if (!this.name.value) {
+      this.name.error = 'Name is required';
+    }
+    else if (!this.expiryMonth.value) {
+      this.expiryMonth.error = 'Please enter month';
+    }
+    else if (!this.expiryYear.value) {
+      this.expiryYear.error = 'Please enter year';
+    }
+    else if (!this.cvv.value) {
+      this.cvv.error = 'Please enter correct CVV';
+    }
+    else {
+      this.name.error = '';
+      this.cardNumber.error = '';
+      this.expiryMonth.error = '';
+      this.expiryYear.error = '';
+      this.cvv.error = '';
+    }
+  }
+
+  // credit(event) {
+  //   this.cardNumber.value+=" "
+  //   console.log(event.target.value);
+  // }
 }
