855004197e1e710d5b23da1896db705045352f78
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-12 02:46:31 +0530

    add-new-card component updated

--- src/app/modules/user-profile/components/add-new-card/add-new-card.component.ts
+++ src/app/modules/user-profile/components/add-new-card/add-new-card.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-add-new-card',
   templateUrl: './add-new-card.component.html',
   styleUrls: ['./add-new-card.component.scss']
 })
+
 export class AddNewCardComponent implements OnInit {
 
   cvv = { value: '', error: '' };
   name = { value: '', error: '' };
   cardNumber = { value: '', error: '' };
-  expiryMonth = { value: '', error: '' };
-  expiryYear = { value: '', error: '' };
+  expiryMonth: any = { value: '', error: '' };
+  expiryYear: any = { value: '', error: '' };
+
+  currentYear = new Date().getFullYear();
+  currentMonth = new Date().getMonth() + 1;
 
   constructor(
-    public dialogRef: MatDialogRef<AddNewCardComponent>
+    public dialogRef: MatDialogRef<AddNewCardComponent>,
   ) { }
 
   ngOnInit(): void {
+    console.log(this.currentYear);
+    console.log(this.currentMonth);
   }
 
   onNoClick(): void {
@@ -26,6 +34,8 @@ export class AddNewCardComponent implements OnInit {
   }
 
   saveCard(): void {
+    console.log(this.currentMonth);
+    console.log(this.currentYear);
     if (!this.cardNumber.value) {
       this.cardNumber.error = 'Card number is required';
     }
@@ -39,7 +49,18 @@ export class AddNewCardComponent implements OnInit {
       this.expiryYear.error = 'Please enter year';
     }
     else if (!this.cvv.value) {
-      this.cvv.error = 'Please enter correct CVV';
+      this.cvv.error = 'Please enter CVV';
+    }
+    else if (this.expiryMonth.value < 1 || this.expiryMonth.value > 12) {
+      this.expiryMonth.error = 'Enter correct month';
+    }
+    else if (this.expiryYear.value < this.currentYear) {
+      this.expiryYear.error = 'Enter correct year';
+    }
+    else if (this.expiryYear.value === this.currentYear) {
+      if (this.expiryMonth.value > this.currentMonth) {
+        this.expiryMonth.error = "Enter correct month"
+      }
     }
     else {
       this.name.error = '';
@@ -50,8 +71,4 @@ export class AddNewCardComponent implements OnInit {
     }
   }
 
-  // credit(event) {
-  //   this.cardNumber.value+=" "
-  //   console.log(event.target.value);
-  // }
 }
