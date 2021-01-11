1027b1648130d8cb5100e8df6b88cbd47c7d45dc
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-12 02:44:11 +0530

    add-address component updated

--- src/app/modules/user-profile/components/add-address/add-address.component.ts
+++ src/app/modules/user-profile/components/add-address/add-address.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocationDataService } from 'src/app/services/location/location-data.service';
+import { ILocation } from 'src/app/shared/interfaces/Ilocation';
+import { locationsList } from 'src/app/shared/locationsList';
+import { mergeMap, startWith, map } from 'rxjs/operators';
+import { myFilter } from 'src/app/modules/home-page/components/home-page-header/home-page-header.component';
+import { Router } from '@angular/router';
+import { RestaurantListService } from 'src/app/services/restaurant-list/restaurant-list.service';
 
 @Component({
   selector: 'app-add-address',
@@ -14,11 +21,74 @@ export class AddAddressComponent implements OnInit {
   address = { value: '', error: '' };
   addressLabel = { value: '', error: '' };
 
+  stateForm: FormGroup = this.formBuilder.group({
+    stateGroup: '',
+  });
+
+  userSearchSelections = {
+    locationName: '',
+    searchName: '',
+    dateTime: ''
+  };
+
+  timeOut: any;
+  stateGroups: ILocation[] = locationsList;
+  stateGroupOptions: any;
+
   constructor(
     public dialogRef: MatDialogRef<AddAddressComponent>,
+    private locationDetaService: LocationDataService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private restaurantLisService: RestaurantListService,
   ) { }
 
-  ngOnInit(): void { }
+  loadCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.locationDetaService.getLocationDetails(position.coords.latitude, position.coords.longitude).subscribe(
+          (details) => {
+            this.userSearchSelections.locationName = details.addresses[0].address.municipality;
+          }
+        );
+      },
+      err => {
+        this.userSearchSelections.locationName = 'Bengaluru';
+      }
+    );
+  }
+
+  searchAction = (event: any) => {
+    clearTimeout(this.timeOut);
+    // var $this = this;
+    this.timeOut = setTimeout(
+      () => {
+        if (event.keyCode !== 13) {
+          this.executeSearch(event.target.value);
+        }
+      }
+      , 1000);
+  }
+
+  executeSearch = (value: any) => {
+    if (!(this.userSearchSelections.searchName === '' || this.userSearchSelections.locationName === '')) {
+      const dateTime: any = this.userSearchSelections.dateTime;
+      if (!(dateTime)) {
+        this.userSearchSelections.dateTime = new Date().toString();
+      }
+
+      this.userSearchSelections.searchName = value;
+      this.locationDetaService.getLatitudeLongitude(this.userSearchSelections.locationName).pipe(
+        mergeMap((coordinates) => this.restaurantLisService.searchRestaurants(this.userSearchSelections, coordinates))
+      ).subscribe((result) => {
+        this.router.navigate(['/restaurant-list']);
+      },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -43,5 +113,24 @@ export class AddAddressComponent implements OnInit {
       this.address.error = '';
       this.addressLabel.error = '';
     }
+  }
+
+  private _filterGroup(value: string): ILocation[] {
+    if (value) {
+      return this.stateGroups
+        .map(group => ({ letter: group.letter, names: myFilter(group.names, value) }))
+        .filter(group => group.names.length > 0);
+    }
+
+    return this.stateGroups;
+  }
+
+  ngOnInit(): void {
+    this.loadCurrentLocation();
+    this.stateGroupOptions = this.stateForm.get('stateGroup')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filterGroup(value))
+      );
   }
 }
