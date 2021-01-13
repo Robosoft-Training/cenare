91159cded0f2bf4eccc452dffad06275cec75780
premrobosoft <premnath@robosoftin.com>
2021-01-13 19:27:02 +0530

    Tracker animation is done

--- src/app/components/shared-components/order-track/order-track.component.ts
+++ src/app/components/shared-components/order-track/order-track.component.ts
@@ -7,8 +7,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./order-track.component.scss']
 })
 export class OrderTrackComponent implements OnInit {
-  formType: any='card';
+
+  formType: any='card0';
   orderID: any;
+  count = 0;
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -16,8 +18,20 @@ export class OrderTrackComponent implements OnInit {
 
   ngOnInit(): void {
     this.orderID = this.activatedRoute.snapshot.paramMap.get('id');
+    this.startTracking();
   }
+
+  startTracking = () => {
+    if(this.count <= 6){
+      setTimeout(() => {
+        this.count++;
+        this.showFormType(`card${this.count}`);
+      }, 2000);
+    }
+  }
+
   showFormType(formName): void {
     this.formType = formName;
+    this.startTracking();
   }
 }
