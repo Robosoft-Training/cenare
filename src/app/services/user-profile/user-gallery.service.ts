2069aee9f34808f7463266af147c356bb3bd3436
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2021-01-13 11:53:44 +0530

    User gallery API connected

--- src/app/services/user-profile/user-gallery.service.ts
+++ src/app/services/user-profile/user-gallery.service.ts
@@ -3,33 +3,33 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { retry, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { LocalStorageService } from '../local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserGalleryService {
 
   // searchData: any;
   apiBaseUrl = environment.baseUrl;
-
   userGalleryDataListSource = new BehaviorSubject({});
   currentUserGalleryDataListSource = this.userGalleryDataListSource.asObservable();
   currentUserGalleryDataList: any;
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient 
   ) { }
 
-  getRestaurantGalleryItems = (): Observable<any> => {
+  getRestaurantGalleryItems = (): Observable<any> => { 
     const url = `${this.apiBaseUrl}userDetails/getGalleryImages`;
+
     return this.httpClient.get<any[]>(url)
       .pipe(
         tap(data => {
-         console.log(data);
+          this.currentUserGalleryDataList = { ...data };
+          // this.galleryDataListSource.next(this.currentGalleryDataList);
         }),
         retry(3)
       );
   }
-
 }
